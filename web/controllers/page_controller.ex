85ba29fb7563a7d9917b891012cf64a0d6069937
Michael.Zemel <michael.zemel@phishme.com>
2016-10-24 20:42:33 -0400

    Remove Page controller, view, template, and spec

--- web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PhoenixImageSvc.PageController do
-  use PhoenixImageSvc.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
--- web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule PhoenixImageSvc.PageView do
-  use PhoenixImageSvc.Web, :view
-end
